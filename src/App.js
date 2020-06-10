a9aa27d2972311035cf4c7f5343a2c71e0f56b8d
Teresa Carbajal <Tere@MacBook-Air-de-Teresa.local>
2020-06-10 16:07:11 -0500

    :lipstick: [Header] adding Header style without functionality

--- src/App.js
+++ src/App.js
@@ -1,15 +1,23 @@
 import React from 'react';
 // Components
 import Routes from './Routes/Routes';
+import Header from './Components/Header/Header';
 // Css
 import './App.css';
 
 function App() {
-  return (
-    <div className='App'>
-      <Routes />
-    </div>
-  );
+    return (
+        <>
+            <Header />
+            <div class="container bg-light mt-about py-5">
+                <div class="row">
+                    <div class="col-lg-6">
+                        <Routes />
+                    </div>
+                </div>
+            </div>
+        </>
+    );
 }
 
 export default App;
