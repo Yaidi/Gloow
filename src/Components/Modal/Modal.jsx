4b43991a09e99e530242004fb2a9ac4f1a718183
Ana Paula Oliva <anapaulafernandez.oliva@gmail.com>
2020-06-11 05:03:04 -0500

    :construction: Work in progress: Updating Composed Component: Modal

--- src/Components/Modal/Modal.jsx
+++ src/Components/Modal/Modal.jsx
@@ -1,12 +1,12 @@
 import React, { useContext, useState } from 'react';
 import { UserContext } from '../../Routes/UserContext'
-import LoginTemplate from './LoginTemplate';
-import ShoppingTemplate from './ShoppingTemplate';
+import LoginTemplate from './Templates/Login/LoginTemplate';
+import ShoppingTemplate from './Templates/Shopping/ShoppingTemplate';
 
 const Modal = () => {
     const { isModalOpen, setIsModalOpen } = useContext(UserContext);
 
-    const [loginAdmin, setLoginAdmin] = useState(false);
+    const [loginAdmin, setLoginAdmin] = useState(true);
 
     const closeModal = () => {
         setIsModalOpen(false);
