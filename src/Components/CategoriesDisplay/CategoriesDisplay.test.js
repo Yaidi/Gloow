0afe2a99b5aba7b7022997fef613c529f563b85e
Ana Paula Oliva <anapaulafernandez.oliva@gmail.com>
2020-06-11 02:09:09 -0500

    :white_check_mark: Adding a test template

--- src/Components/CategoriesDisplay/CategoriesDisplay.test.js
+++ src/Components/CategoriesDisplay/CategoriesDisplay.test.js
@@ -1,1 +1,12 @@
-/* test */
+import React from 'react';
+import { render, fireEvent } from '@testing-library/react';
+import CategoriesDisplay from './CategoriesDisplay';
+
+//test suite
+describe('test suite for CategoriesDisplay component', () => {
+    //test cases
+    test('description of the test purpose', () => {
+        expect()/*.toBeInTheDocument()*/
+    });
+
+});
--- src/Components/Footer/Footer.test.js
+++ src/Components/Footer/Footer.test.js
@@ -1,1 +1,12 @@
-/* test */
+import React from 'react';
+import { render, fireEvent } from '@testing-library/react';
+import Footer from './Footer';
+
+//test suite
+describe('test suite for footer component', () => {
+    //test cases
+    test('description of the test purpose', () => {
+        expect()/*.toBeInTheDocument()*/
+    });
+
+});
