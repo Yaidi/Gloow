6764ff922720d4a0e0fd2b3da2001839e95a53f5
biancardona <biancardona.rivera@gmail.com>
2020-06-11 11:36:30 -0500

    Adding Tere cahnges

--- src/View/Home.jsx
+++ src/View/Home.jsx
@@ -1,17 +1,17 @@
 import React from "react";
 // Components
 import SearchForm from "../Components/SearchButton/SearchButton";
-<<<<<<< Updated upstream
+
 import ContainerPromotion from "../Components/ContainerPromotion/ContainerPromotion";
 import { CategoriesDisplay } from "../Components/CategoriesDisplay/CategoriesDisplay";
-=======
+
 import CategoriesDisplay from "../Components/CategoriesDisplay/CategoriesDisplay";
->>>>>>> Stashed changes
+
 import PromotionsDisplay from "../Components/PromotionsDisplay/PromotionsDisplay";
 
 const Home = () => {
     return (
-<<<<<<< Updated upstream
+
         <>
             <div className="row">
                 <div className="col-lg-12 d-flex justify-content-end">
@@ -32,13 +32,7 @@ const Home = () => {
                 </div>
             </div>
         </>
-=======
-        <div>
-            <SearchForm />
-            <PromotionsDisplay />
-            <CategoriesDisplay />
-        </div>
->>>>>>> Stashed changes
+
     );
 };
 
