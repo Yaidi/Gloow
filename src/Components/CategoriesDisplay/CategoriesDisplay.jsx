acc3b88d2cfdc82674652920ed205379f5837239
biancardona <biancardona.rivera@gmail.com>
2020-06-10 23:52:27 -0500

    :sparkles: Adding category images in view Home and is working the route for that categories, adding the Fetch for ProductsSearchbyCategory

--- src/Components/CategoriesDisplay/CategoriesDisplay.jsx
+++ src/Components/CategoriesDisplay/CategoriesDisplay.jsx
@@ -1,6 +1,13 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import { useState, useEffect } from "react";
 import { AllCategories } from "../../Library/UseFetch";
+import womanl from "../../Assets/categoriesImages/womanl.jpg";
+import handbags from "../../Assets/categoriesImages/handbags.jpg";
+import shoes from "../../Assets/categoriesImages/shoes.jpg";
+import jewerlyandaccesories from "../../Assets/categoriesImages/jewerlyandaccesories.jpg";
+import kids from "../../Assets/categoriesImages/kids.jpg";
+import home from "../../Assets/categoriesImages/home.jpg";
 
 export default function CategoriesDisplay() {
     const [data, setData] = useState([]);
@@ -24,12 +31,41 @@ export default function CategoriesDisplay() {
             categoriesBuffer.push(randomElementsArray[0]);
         }
         randomCategories = categoriesBuffer;
+        for (let i = 0; i < randomCategories.length; i++) {
+            switch (randomCategories[i].description) {
+                case "Woman":
+                    randomCategories[i].picture = womanl;
+                    break;
+                case "Handbags":
+                    randomCategories[i].picture = handbags;
+                    break;
+                case "Shoes":
+                    randomCategories[i].picture = shoes;
+                    break;
+                case "Jewerly & Accesories":
+                    randomCategories[i].picture = jewerlyandaccesories;
+                    break;
+                case "Kids":
+                    randomCategories[i].picture = kids;
+                    break;
+                case "Home":
+                    randomCategories[i].picture = home;
+                    break;
+                default:
+                    break;
+            }
+        }
     }
     return (
         <div>
             {randomCategories.map((el, index) => (
                 <section key={index}>
-                    <div>{el.description}</div>
+                    <div>
+                        <Link to={`/CategoriesList/${el.categoryId}`}>
+                            <img src={el.picture} alt="" />
+                            {el.description}
+                        </Link>
+                    </div>
                 </section>
             ))}
         </div>
