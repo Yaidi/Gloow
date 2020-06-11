70a85da1c76a4ed1a6ba89215c2d43372cc69bbf
Yaidi <yaidi.garcia.lopez@gmail.com>
2020-06-10 20:14:43 -0500

    :lipstick: Create counter and put in Header with style

--- src/Components/CarshoppingButton/CarshoppingButton.jsx
+++ src/Components/CarshoppingButton/CarshoppingButton.jsx
@@ -1,20 +1,18 @@
-import React, {useState} from 'react'
-import ShoppingIcon from '../../Assets/icons/shopping-icon.png'
-import Style from './CarshoppingButton.module.css'
+import React, {useState} from "react"
+import Style from "./CarshoppingButton.module.css"
 
-function upCounter (){
-'a'
-}
-const ShoppingButton = (clic) =>{
-    const [counter, setCounter] = useState('0');
+/*function upCounter (action){
+    if(action === "add"){ }
+}*/
+const ShoppingButton = (clic, action) =>{
+    const [counter, setCounter] = useState("0");
     
+    
+
     return(
-        <div className={Style.container}>
-            <img src={ShoppingIcon} className={Style.bag} alt='Button shoping car'/>
             <span className={Style.counter}>
                 {counter}
             </span>
-        </div>
     )
 }
 
