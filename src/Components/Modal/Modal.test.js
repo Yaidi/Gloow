c3026be3c2abbed1f37328f4cf4826d5b27364c2
Ana Paula Oliva <anapaulafernandez.oliva@gmail.com>
2020-06-11 20:07:53 -0500

    :rotating_light: Adding tests

--- src/Components/Modal/Modal.test.js
+++ src/Components/Modal/Modal.test.js
@@ -2,6 +2,19 @@ import React from 'react';
 import { render, fireEvent } from '@testing-library/react';
 import Modal from './Modal';
 
+/*  motto: facilitate querying the DOM in the same way the user would.
+    What we want to test: 
+    From modal: 
+    -Close modal button function to be called
+
+    From loginTemplate:
+    -Close modal icon to exist
+    -Inputs to be validated
+
+    From ShoppingTemplate:
+    -Close modal icon to exist
+    -Process order function to be called */
+
 //test suite
 describe('test suite for modal component', () => {
     //test cases
