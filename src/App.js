d09f537d424625b99f083e8cfaed3a9a89862d40
Yaidi García <56900133+Yaidi@users.noreply.github.com>
2020-06-12 01:58:23 -0500

    Delete import Header

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import React from 'react';
 
 import Routes from './Routes/Routes';
-import Header from './Components/Header/Header';
 import Footer from './Components/Footer/Footer';
 import { UserContextProvider } from "./Routes/UserContext";
 import Modal from './Components/Modal/Modal'
@@ -11,7 +10,6 @@ function App() {
     return (
         <>
             <UserContextProvider>
-                 <Header/>
                 <div className="container-fluid body-content back py-5">
                     <div className="container back">
                         <Routes />
