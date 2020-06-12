b013262b6a658445dc3982ceb019dc7db6ad5fd2
Teresa Carbajal <Tere@MacBook-Air-de-Teresa.local>
2020-06-11 23:50:17 -0500

    Resolving conflicts

--- src/View/ProductsDescription.jsx
+++ src/View/ProductsDescription.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import {ProductsDetails} from "../Components/ProductsDisplay/ProductsDisplay";
+import { ProductsDetails } from "../Components/ProductsDisplay/ProductsDisplay";
 import { Link } from "react-router-dom";
 import { ProductsId } from "../Library/UseFetch";
 import BackTo from "../Components/LinkBackTo/LinkBackTo";
@@ -10,25 +10,30 @@ export default function ProductsDescription({ match }) {
     console.log(data);
     if (data !== undefined) {
         return (
-            <div>
-      <BackTo move="/" nameback="HOME"/>
-  
-            <div>
-                <ul>
-                    <li>
-                        <Link
-                            key={data.productId}
-                            to={`/ProductsList/${data.productId}`}
-                        >
-                            <p>{data.description}</p>
-                            <p>{data.colors}</p>
-                            <p>{data.sizes}</p>
-                            <p>{data.price}</p>
-                        </Link>
-                    </li>
-                </ul>
-             </div>
-            </div>
+            <>
+                <BackTo move="/" nameback="HOME" />
+
+                <div className="container">
+                    <div className="row">
+                        <div className="col-md-6 col-lg-4 my-3">
+                            <div className="card text-center">
+                                <section className="card-body back letter-white" mb-3 >
+                                    <Link
+                                        key={data.productId}
+                                        to={`/ProductsList/${data.productId}`}
+                                    >
+                                        <p>{data.colors}</p>
+                                        <p>{data.sizes}</p>
+                                        <p>{data.description}</p>
+                                        <p>{data.price}</p>
+                                    </Link>
+                                    <a href="#" className="btn btn-secondary">Go somewhere</a>
+                                </section>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </>
         );
     }
     return (
