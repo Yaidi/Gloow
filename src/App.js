61d69d0cfed643f4f1c38ceded4152e606f29812
Yaidi García <56900133+Yaidi@users.noreply.github.com>
2020-06-12 00:41:29 -0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -14,8 +14,8 @@ function App() {
     return (
         <>
             <UserContextProvider>
+                 <Header/>
                 <div className="container-fluid body-content back py-5">
-                        <Header/>
                     <div className="container back">
                         <Routes />
                     </div>
