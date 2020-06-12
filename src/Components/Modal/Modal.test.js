8a7fd89e5bbb09247bd24458d3a3d0029b2ca976
Ana Paula Oliva <anapaulafernandez.oliva@gmail.com>
2020-06-11 21:04:50 -0500

    :heavy_check_mark: Make tests pass

--- src/Components/Modal/Modal.test.js
+++ src/Components/Modal/Modal.test.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import { render, fireEvent } from '@testing-library/react';
 import Modal from './Modal';
+import LoginTemplate from './Templates/Login/LoginTemplate';
+import ShoppingTemplate from './Templates/Shopping/ShoppingTemplate';
 
 /*  motto: facilitate querying the DOM in the same way the user would.
     What we want to test: 
-    From modal: 
-    -Close modal button function to be called
 
     From loginTemplate:
     -Close modal icon to exist
@@ -16,10 +16,41 @@ import Modal from './Modal';
     -Process order function to be called */
 
 //test suite
-describe('test suite for modal component', () => {
+describe('test suite for modal templates', () => {
     //test cases
-    test('description of the test purpose', () => {
-        expect()/*.toBeInTheDocument()*/
+    test('Close modal icon to be in the document', () => {
+        const { container } = render( <LoginTemplate/> );
+        expect( container.querySelector(".close-button")).toBeInTheDocument();
     });
 
+    test('Input email to be validated', () => {
+        const { container } = render( <LoginTemplate/> );
+        expect( container.querySelector("#email")).toBeValid();
+    });
+
+    test('Input password to be validated', () => {
+        const { container } = render( <LoginTemplate/> );
+        expect( container.querySelector("#password")).toBeValid();
+    });
+
+    test('Login validation function to be called', () => {
+        const { container } = render( <LoginTemplate/> );
+        //function fn.jest()
+        const loginButton = container.querySelector(".LoginButton");
+        fireEvent.click(loginButton);
+        expect(/*function*/).toHaveBeenCalled();
+    });
+
+    test('Close modal icon to be visible', () => {
+        const { container } = render( <ShoppingTemplate/> );
+        expect( container.querySelector(".close-button")).toBeVisible();
+    });
+
+    test('Process-order function to be called', () => {
+        const { container } = render( <LoginTemplate/> );
+        //function fn.jest()
+        const processOrder = container.querySelector(".OrderButton");
+        fireEvent.click(processOrder);
+        expect(/*function*/).toHaveBeenCalled();
+    });
 });
